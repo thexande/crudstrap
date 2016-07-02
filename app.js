c41fb0339d2235b0fbf65b853fdc1d25de3c328d
Alex <amurphy345@gmail.com>
2016-07-02 15:21:46 -0600

    dependency install working

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var fs = require('fs-extra')
+var sys = require('sys')
+var exec = require('child_process').exec;
 var serverCtrl = require('./controllers/server')
 // import our configuration file
 
 var crudstrapConfig = fs.readJsonSync('./crudstrap.json')
-console.log(crudstrapConfig);
+// console.log(crudstrapConfig);
 // generate serverside files
 serverCtrl.generateServerFiles(crudstrapConfig)
 
+// download dependencies
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+exec('cd ' + crudstrapConfig.name + '&& npm install && bower install', puts);
 
 // var packageObj = {"property":"value"}
 // fsp.writeFile("./projectDirectory/package.json", JSON.stringify(packageObj))
